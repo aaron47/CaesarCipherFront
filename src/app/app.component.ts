0e4fb1f7b84fc91c37cf8ff1b371c6004c880393
aaron47 <aaronborgi1@gmail.com>
2023-10-25 17:21:34 +0100

    fixed commonjs problems and added more styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,7 @@
-import {CaesarCipherService} from './services/caesar-cipher.service';
-import {Component, DestroyRef} from '@angular/core';
-import {CaesarPolyalphabeticService} from './services/caesar-polyalphabetic-service';
-import {ReplacementService} from './services/replacement-service';
-
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-}
+export class AppComponent {}
